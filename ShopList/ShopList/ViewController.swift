2032bf812379d497213c3ab9ecf26c9a2d06d213
thiago <thiagoamaral.oliveira@gmail.com>
2022-03-16 17:08:42 -0300

    created projecy

---
+++ ShopList/ShopList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopList
+//
+//  Created by Aluno18 on 16/03/22.
+//  Copyright © 2022 Aluno18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
